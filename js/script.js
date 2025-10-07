8107fc616a89ed88271d411cffa3a1d307bfc33e
Lucas <Lucas.f.lima37@aluno.senai.br>
2025-10-07 17:27:38 -0300

    scrit produtos

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const sombra = document.getElementById("sombra");
+
+function mostrarMenu(){
+//   se
+    if (window.getComputedStyle(sombra).top != "-210px"){
+        //se sim
+         sombra.style.top = "-210px";
+
+    } else{//se não
+        sombra.style.top = "0";
+    // não
+    }
+
+   
+   
+}
